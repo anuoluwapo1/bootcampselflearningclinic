9f636193f0b5494e925811c72c2a00caea3ad56e
Anu Onifade <anuonifade@gmail.com>
2017-02-10 16:09:16 +0100

    Changes to make variables more understandable and a few comment to make code readable

--- question3/app/library.js
+++ question3/app/library.js
@@ -7,37 +7,37 @@ module.exports = {
 		if(numbers.length < 1){
 			return 0;
 		}
-		var a = numbers[0];
-		var arithProgression;
-		var d = numbers[1] - numbers[0];
-		var r = numbers[1] / numbers[0];
-		var i = 0;
-		var ap = true;
-		var gp = true;
-		for(i = 0; i < numbers.length - 1; i++){
-			if(numbers[i] === (a + (i * d))){
+		var firstTerm = numbers[0]; //First term a of an Arithmetic or Geometric Progression
+		var diffAP = numbers[1] - numbers[0]; //Difference between nth terms of an Arithmetic Progression
+		var ratio = numbers[1] / numbers[0]; //Ratio between nth terms of a Geometric Progression
+		var nth; //This will be (n-1) term of the sequence since sequence starts from 1 and array starts from 0
+		var isArithProgression = true; 
+		var isGeoProgression = true;
+		for(nth = 0; nth < numbers.length - 1; nth++){
+			if(numbers[nth] === (firstTerm + (nth * diffAP))){ //Using the formula for AP  - nth_term = [a + (n-1)d] where (n - i) = nth
 				continue;
 			}
 			else{
-			  ap = false;
+			  isArithProgression = false; //If an element in the array is out of sequence. The array is not an AP and break the loop
 			  break;
 			}
 		}
-		if(ap === true){
+		if(isArithProgression === true){ 
 			return 'Arithmetic';
 		}
+		//If the sequence is not an Arithmetic Progression, test if it a Geometric Progression
 		else{
-			for(i = 0; i < numbers.length -1; i++){
-		  		if(numbers[i] === (a * Math.pow(r, i))){
+			for(nth = 0; nth < numbers.length - 1; nth++){
+		  		if(numbers[nth] === (firstTerm * Math.pow(ratio, nth))){
 		  			continue;
 		  		}
 		  		else{
-		  			gp = false;
+		  			isGeoProgression = false;
 		  			break;
 		  		}
 		  	}
 		  
-		  	if(gp === true){
+		  	if(isGeoProgression === true){
 	  			return 'Geometric';
 	  		}
 	  		else{
